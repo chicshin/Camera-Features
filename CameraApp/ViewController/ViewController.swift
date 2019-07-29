85869d61ab01182e938638dbbb59068c3ebec398
Chicshin <inchicshin@gmail.com>
2019-07-29 21:19:44 +0900

    Implemented swich front/back camera and ZoomIn/ZoomOut features

--- CameraApp/ViewController/ViewController.swift
+++ CameraApp/ViewController/ViewController.swift
@@ -16,22 +16,49 @@ class ViewController: UIViewController {
     var captureSession = AVCaptureSession()
     var backCamera: AVCaptureDevice?
     var frontCamera: AVCaptureDevice?
-    var currentCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice?
     
     var photoOutput: AVCapturePhotoOutput?
     
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
     var image: UIImage?
+    
+    var toggleCameraGestureRecognizer = UISwipeGestureRecognizer()
+    
+    var zoomInGestureRecognizer = UISwipeGestureRecognizer()
+    var zoomOutGestureRecognizer = UISwipeGestureRecognizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        shotButton.layer.cornerRadius = shotButton.frame.width/2
         
         setupCaptureSession()
         setupDevice()
         setupInputOutput()
         setupPreviewLayer()
-        startRunningCaptureSession()
+        captureSession.startRunning()
+        
+        toggleCameraGestureRecognizer.direction = .up
+        toggleCameraGestureRecognizer.addTarget(self, action: #selector(self.switchCamera))
+        view.addGestureRecognizer(toggleCameraGestureRecognizer)
+        
+        zoomInGestureRecognizer.direction = .right
+        zoomInGestureRecognizer.addTarget(self, action: #selector(self.zoomIn))
+        view.addGestureRecognizer(zoomInGestureRecognizer)
+        
+        zoomOutGestureRecognizer.direction = .left
+        zoomOutGestureRecognizer.addTarget(self, action: #selector(self.zoomOut))
+        view.addGestureRecognizer(zoomOutGestureRecognizer)
+        styleCaptureButton()
+    }
+    
+    func styleCaptureButton(){
+        shotButton.layer.borderColor = UIColor.white.cgColor
+        shotButton.layer.borderWidth = 5
+        shotButton.clipsToBounds = true
+        shotButton.layer.cornerRadius = shotButton.frame.width/2
+        
+
     }
     
     //resolution settings
@@ -49,13 +76,13 @@ class ViewController: UIViewController {
             }else if device.position == AVCaptureDevice.Position.front{
                 frontCamera = device
             }
-            currentCamera = backCamera
+            currentDevice = backCamera
         }
     }
     
     func setupInputOutput(){
         do {
-            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
             captureSession.addInput(captureDeviceInput)
             photoOutput = AVCapturePhotoOutput()
             photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
@@ -72,8 +99,59 @@ class ViewController: UIViewController {
         self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
     }
     
-    func startRunningCaptureSession(){
-        captureSession.startRunning()
+    @objc func switchCamera(){
+        captureSession.beginConfiguration()
+        
+        let newDevice = (currentDevice?.position == AVCaptureDevice.Position.back) ? frontCamera: backCamera
+        
+        for input in captureSession.inputs {
+            captureSession.removeInput(input as! AVCaptureDeviceInput)
+        }
+        
+        let cameraInput:AVCaptureDeviceInput
+        do {
+            cameraInput = try AVCaptureDeviceInput(device: newDevice!)
+        }catch {
+            print(error)
+            return
+        }
+        
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        }
+        
+        currentDevice = newDevice
+        captureSession.commitConfiguration()
+    }
+    
+    @objc func zoomIn() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor < 5.0 {
+                let newZoomFactor = min(zoomFactor + 1.0, 5.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                }catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    @objc func zoomOut() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor > 1.0 {
+                let newZoomFactor = max(zoomFactor - 1.0, 1.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                }catch {
+                    print(error)
+                }
+            }
+        }
     }
     
     @IBAction func shotButton_touchUpInside(_ sender: Any) {
